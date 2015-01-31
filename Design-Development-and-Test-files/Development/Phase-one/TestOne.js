1f87a22b7a4bf6e91230ba94cff349d4067712e4
James <jammim19@gmail.com>
2015-01-31 00:05:33 +0000

    Defining the functionality for displaying cubes

---
+++ Design-Development-and-Test-files/Development/Phase-one/TestOne.js
@@ -0,0 +1,25 @@
+//Represents the basic functionality of displaying multiple cubes
+
+function Init(){
+
+}//Init
+
+function animate(){
+	
+}//animate
+
+function createCubes(){
+
+loop(100){
+ 
+ //add new Cube and define random Position / cube
+
+}
+
+}//createCubes
+
+
+
+function Draw(){
+	
+}//Draw
