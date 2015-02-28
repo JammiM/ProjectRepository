04ae212d1072cef40f2c88f67a7afaf5001d4790
James <jammim19@gmail.com>
2015-02-28 21:54:27 +0000

    Added more drivers and and libs, updated code

--- Design-Development-and-Test-files/Development/Phase-one/OpenGL/Select version/main.cpp
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/Select version/main.cpp
@@ -7,24 +7,23 @@ using namespace std;
 
 int  main ()
 {
-
-glewExperimental = GL_TRUE;
-GLenum err = glewInit();
-if (GLEW_OK != err) {
-    std::cerr<<"Error: "<<glewGetErrorString(err)<<std::endl;
-} else {
-    if (GLEW_VERSION_4_1)
-    {
-        std::cout<<"Driver supports OpenGL 2.0\nDetails:"<<std::endl;
-    }
-}
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err){
+	    std::cerr<<"Error: "<<glewGetErrorString(err)<<std::endl;
+	} else {
+	    if (GLEW_VERSION_2_0)
+	    {
+	        std::cout<<"Driver supports OpenGL 2.0\nDetails:"<<std::endl;
+	    }
+	}
 
 	glutInit(&__argc,__argv);
 	glutInitDisplayMode(GLUT_SINGLE);
     glutInitContextVersion(2,0);  
 	glutInitWindowSize(500,500);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("OpenGL Test");
+	glutCreateWindow("OpenGL Version Test");
 	glutDisplayFunc(display);
 	glutMainLoop();
 
