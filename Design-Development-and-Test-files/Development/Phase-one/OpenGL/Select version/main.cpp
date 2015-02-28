1a4e4228e02f6c31dacbac57066d16c2d77273da
James <jammim19@gmail.com>
2015-02-28 18:15:25 +0000

    Added visual studio proj to target 2.0

---
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/Select version/main.cpp
@@ -0,0 +1,39 @@
+//This simple class is used to define a specific version of OpenGL
+//The specific version is 2.0.
+
+#include <GL/freeglut.h>
+
+using namespace std;
+
+int  main ()
+{
+
+glewExperimental = GL_TRUE;
+GLenum err = glewInit();
+if (GLEW_OK != err) {
+    std::cerr<<"Error: "<<glewGetErrorString(err)<<std::endl;
+} else {
+    if (GLEW_VERSION_4_1)
+    {
+        std::cout<<"Driver supports OpenGL 2.0\nDetails:"<<std::endl;
+    }
+}
+
+	glutInit(&__argc,__argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+    glutInitContextVersion(2,0);  
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("OpenGL Test");
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+    return 1;
+}
+
+
+
+//glGetString​(GL_VENDOR​);// vendor name
+//glGetString​(GL_VERSION​);
+//glGetString​(GL_RENDERER​);
+//glGetString​(GL_SHADING_LANGUAGE_VERSION​​);
