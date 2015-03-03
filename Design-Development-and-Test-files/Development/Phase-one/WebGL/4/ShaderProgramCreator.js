a9572c22c9f823f0907019c2713bdd1d7c91e474
James Morrissey Jr <jammim19@gmail.com>
2015-03-03 18:05:21 +0000

    Added pseudocode for creating a cleaned "S program"

---
+++ Design-Development-and-Test-files/Development/Phase-one/WebGL/4/ShaderProgramCreator.js
@@ -0,0 +1,22 @@
+
+//This is a simple alert dialog displaying the error and line number.
+window.onerror = function(msg, lineno, url){
+	alert(url + "\n" + "Line number " + lineno + "\n" + msg);
+}
+
+
+function createShader(shader, scorce){
+	
+	return shader
+}//createShader
+
+
+function createShaderProgram(vertexShader, fragmentShader){
+	
+	//attach vertexShader
+	//attach fragmentShader
+	
+	//linkProgram
+	
+	return program
+}//createShaderProgram
