8b9e48bfe925bd70183c2714fa199a352bfe23e4
James <jammim19@gmail.com>
2015-03-08 23:15:10 +0000

    Cleaning code

--- Design-Development-and-Test-files/Development/Phase-one/WebGL/4/ShaderProgramCreator.js
+++ Design-Development-and-Test-files/Development/Phase-one/WebGL/4/ShaderProgramCreator.js
@@ -1,26 +1,24 @@
-
 //This is a simple alert dialog displaying the error and line number.
 window.onerror = function(msg, lineno, url){
 	alert(url + "\n" + "Line number " + lineno + "\n" + msg);
 }
 
-
 function createShader(source, shaderType){
 	var shader = gl.createShader(shaderType);
-	gl.shaderSource(shaderType, source);
+	gl.shaderSource(shader, source);
 	gl.compileShader(shader);
 	
-	return shader
+	return shader;
 }//createShader
 
 
-function createShaderProgram(vertexShaderSource, fragmentShaderSource){
+function createProgram(vertexShaderSource, fragmentShaderSource){
 	var program = gl.createProgram();
 	var vertexShader = createShader(vertexShaderSource, gl.VERTEX_SHADER);
 	var fragmentShader = createShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
 	gl.attachShader(program, vertexShader);
 	gl.attachShader(program, fragmentShader);
 	gl.linkProgram(program);
-	
-	return program
-}//createShaderProgram
+
+	return program;
+}//createProgram
