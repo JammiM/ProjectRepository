aa3cd71295f32e646a0492a6b4a0c91318de0a9c
James Morrissey Jr <jammim19@gmail.com>
2015-03-10 20:11:41 +0000

    Updated shader functionality, and main()

--- Design-Development-and-Test-files/Development/Phase-one/OpenGL/VAO Code/main.cpp
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/VAO Code/main.cpp
@@ -1,7 +1,22 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-main () {
+
+int main(int argc, char** argv) {
+	glutInit(argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(600,600);
+	glutInitContextVersion(2,0);
+	glutInitContextFlags (GLUT_CORE_PROFILE | GLUT_DEBUG);
+	glutInitContextProfile(GLUT_FORWARD_COMPATIBLE);
+	glutCreateWindow(argv[0]);
+	
+	if (glew)
+}//main
+
+
+
+void BuildGeometry() {
 	GLuint vertexArrayBuffer;
 	GLuint vertexBufferObject;
 	GLunit numVertices;
@@ -21,11 +36,36 @@ main () {
 	glBufferSubData(GL_ARRAY_BUFFER, numVertices*3*sizeof(GLFLOAT), numVertices*4*sizeof(GLFLOAT), colours);
 	
 	//Shader stuff
-	vertexPositionID = glGetAttribLocation();
-	colourOfVertexID = glGetAttribPointer();
-	glUseProgram();
-	glEnableVertexAttribArray();
-	glEnableVertexAttribArray();
-
+	vertexPositionID = glGetAttribLocation(vertexPositionID, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	colourOfVertexID = glGetAttribPointer(colourOfVertexID, 4, FLOAT,GL_FALSE, 0,
+	BUFFER_OFFSET(3*3sizeof(GLFLOAT)));
+	glUseProgram(shaderProgram);
+	glEnableVertexAttribArray(vertexPositionID);
+	glEnableVertexAttribArray(colourOfVertexID);
+	
+	
+	//flush 
+	//glFinish(); / glFlush();
 	
 }//main
+
+
+shaderStuff() {
+	GLuint vpos;
+
+	vpos = glGetAttribLocation(programID, "vPosition");
+	glEnableVertexAttribArray(vpos);
+	glVertexAttribPointer(vpos, 3, GL_FLOAT, GL_FALSE, 0, 0);//Location of the attribute
+}//shaderStuff
+
+vShader() {
+	vec4 vPosition
+	vec4 vColour;
+	vec4 colour;
+
+	void main() {
+	 colour = vColour;
+	 gl_Position = vPosition;
+	}
+}//vShader
+
