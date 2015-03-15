ffaf89cbac6c3282261086598f187b0938348e4d
James <jammim19@gmail.com>
2015-03-15 14:07:04 +0000

    Added temp GLSL

---
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/3/Shaders/OpenGL/OpenGL/LPlainPolygonProgram2D.h
@@ -0,0 +1,23 @@
+#ifndef LPLAIN_POLYGON_PROGRAM_2D_H
+#define LPLAIN_POLYGON_PROGRAM_2D_H
+
+#include "LShaderProgram.h"
+
+class LPlainPolygonProgram2D : public LShaderProgram
+{
+	public:
+		bool loadProgram();
+        /*
+        Pre Condition:
+         -A valid OpenGL context
+        Post Condition:
+         -Loads plain polygon program
+        Side Effects:
+         -None
+        */
+
+	private:
+
+};
+
+#endif
