54ce319df09746131d6e566bc1e316880764630f
James Morrissey Jr <jammim19@gmail.com>
2015-04-27 20:44:57 +0100

    removing Qt

--- Design-Development-and-Test-files/Development/Phase-one/OpenGL/Qt/Base/drivers/Qt/include/QtGui/qapplication.h
+++
@@ -1,5 +0,0 @@
-#include "../../src/gui/kernel/qapplication.h"
-//#include "QtGui/qapplication.h"
-
-//#include file "C:\Users\James\Desktop\zz\OGL\..\drivers\Qt\include\QtGui/qapplication.h" includes itself	
-//c:\Users\James\Desktop\zz\drivers\Qt\include\QtGui\qapplication.h	2	1	OGL
