5e8ce4dd6acbe66870df884a689f8764a735071f
James <jammim19@gmail.com>
2015-05-13 15:04:13 +0100

    Updating ShaderManger

--- Design-Development-and-Test-files/Development/Phase-one/OpenGL/TestLine/Project1/shaderManager.cpp
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/TestLine/Project1/shaderManager.cpp
@@ -8,4 +8,29 @@ shaderManager::shaderManager(void)
 
 shaderManager::~shaderManager(void)
 {
+	glDeleteProgram();
+	glDeleteShader();
+	
+}
+
+shaderManager::createShader(){
+	//type and source.
+	//glCreateShader();
+	
+	//glShaderSource();
+	//glIsShader();
+//	void glCompileShader(GLuint shader);
+
+	//error handling
+}
+
+
+shaderManager::initShaderProgram(){
+	glCreateProgram();
+	glAttachShader();
+	glAttachShader();
+	glLinkProgram();
+	glUseProgram();	
+	   glGetProgramInfoLog();
+	glIsProgram();
 }
--- Design-Development-and-Test-files/Development/Phase-one/OpenGL/TestLine/Project1/shaderManager.h
+++ Design-Development-and-Test-files/Development/Phase-one/OpenGL/TestLine/Project1/shaderManager.h
@@ -1,8 +1,33 @@
 #pragma once
+
+#ifndef SHADERMANAGER_H
+#define SHADERMANAGER_H
+
 class shaderManager
 {
 public:
 	shaderManager(void);
 	~shaderManager(void);
+	
+	
+	createShader();
+	
+	
+	loadShader();
+	initShaderProgram();
+	Gluint getShaderId();
+	
+	
+private:
+    GLuint shaderId;
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    const GLchar* vertexShaderSource;
+    const GLchar* fragmentShaderSource;
+
+    GLint modelViewProjectionMatrixId;
+    GLint viewMatrixId;	
+	
 };
 
+#endif
