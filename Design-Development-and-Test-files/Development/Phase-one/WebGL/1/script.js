7f30e2884104be8e3359840b4f0cb542b12c6ebb
James <jammim19@gmail.com>
2015-08-11 23:19:45 +0100

    Updating Proj

---
+++ Design-Development-and-Test-files/Development/Phase-one/WebGL/1/script.js
@@ -0,0 +1,23 @@
+"using strict";
+
+var canvas;
+var gl;
+
+window.onload = function init()
+{
+}//init
+
+function update()
+{
+  render();
+}//update
+
+function render() {
+  window.requestAnimationFrame(render);
+}//render
+
+function loadShader(id, type)
+{
+  return shader;
+}//loadShader
+
